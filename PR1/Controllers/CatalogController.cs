f86b2138cbd96e04074da7524a1571e5a58c72e4
Evgeniy Novikov <isip_e.e.novikov@mpt.ru>
2025-05-31 22:54:38 +0300

    Added Search and Sort to CatalogPage

--- PR1/Controllers/CatalogController.cs
+++ PR1/Controllers/CatalogController.cs
@@ -12,9 +12,48 @@ public class CatalogController : Controller
         _context = context;
     }
 
-    public async Task<IActionResult> Index()
+    public async Task<IActionResult> Index(string searchTerm = "", string sortBy = "", string sortOrder = "asc")
     {
-        var products = await _context.Products.ToListAsync();
+        var productsQuery = _context.Products.AsQueryable();
+
+        // Поиск по названию (без учета регистра)
+        if (!string.IsNullOrEmpty(searchTerm))
+        {
+            productsQuery = productsQuery.Where(p => p.Name.ToLower().Contains(searchTerm.ToLower()));
+        }
+
+        // Сортировка
+        switch (sortBy.ToLower())
+        {
+            case "price":
+                productsQuery = sortOrder == "desc" 
+                    ? productsQuery.OrderByDescending(p => p.Price)
+                    : productsQuery.OrderBy(p => p.Price);
+                break;
+            case "date":
+                productsQuery = sortOrder == "desc" 
+                    ? productsQuery.OrderByDescending(p => p.Id)
+                    : productsQuery.OrderBy(p => p.Id);
+                break;
+            case "name":
+                productsQuery = sortOrder == "desc" 
+                    ? productsQuery.OrderByDescending(p => p.Name)
+                    : productsQuery.OrderBy(p => p.Name);
+                break;
+            default:
+                productsQuery = productsQuery.OrderBy(p => p.Id);
+                break;
+        }
+
+        var products = await productsQuery.ToListAsync();
+
+        // Передаем параметры в представление
+        ViewBag.SearchTerm = searchTerm;
+        ViewBag.SortBy = sortBy;
+        ViewBag.SortOrder = sortOrder;
+        ViewBag.HasResults = products.Any();
+        ViewBag.IsSearching = !string.IsNullOrEmpty(searchTerm);
+
         return View(products);
     }
 }
--- PR1/obj/Debug/net8.0/PR1.AssemblyInfo.cs
+++ PR1/obj/Debug/net8.0/PR1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PR1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71388e054a16aa89beb0583af003f4c0810ada73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4617c106aaf97c4c8783dd316b974efe1a867909")]
 [assembly: System.Reflection.AssemblyProductAttribute("PR1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PR1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
